51840f8eee106478b7631d1be2347c66c6333d45
Schmel924 <Schmel924@gmail.com>
2017-04-21 15:27:55 +0300

    changed ptr to obj

--- citymap.h
+++ citymap.h
@@ -6,17 +6,23 @@
 
 #define MAX_BLOCK_AXIS 10
 class cityblock {
-private:
+public:
 	std::string name;
-	int something;
-	int somethingelse;
+	int A;
+	int B;
+	cityblock (std::string _name = "Empty\n",int _a=0,int _b=0){name=_name;A=_a;B=_b;}
 };
 
 class citymap {
 public:
-	cityblock * map [MAX_BLOCK_AXIS][MAX_BLOCK_AXIS];
+	cityblock  map [MAX_BLOCK_AXIS][MAX_BLOCK_AXIS];
 	const static int max_block_axis = 5; //well, shit	
-void showcityblock(sf::Vector2i);
+        void showcityblock(sf::Vector2i);
+	citymap() 
+	{static cityblock CBlock;
+	for (int i =0;i<MAX_BLOCK_AXIS;i++)
+		for(int j=0;j<MAX_BLOCK_AXIS;j++)
+			{map[i][j]=CBlock;}}
 };
 
 #endif //_CITYMAP_H_
